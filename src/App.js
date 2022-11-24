d0e1d4501d03aabdc6928d14052045623bbf6f14
anhasamea <anhasamea707@gmail.com>
2022-11-24 16:30:38 +0600

    navbar created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
-import './App.css';
+import {RouterProvider} from "react-router-dom";
+import router from './Router/Routes/Routes.js';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcomegit</h1>
+    <div className="max-w-7xl mx-auto text-slate-800">
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+// import React, { useContext } from 'react';
+// import { Link,useNavigate  } from 'react-router-dom';
+import logo from '../../../assets/images/logo.png'
+// import { AuthContext } from '../../../context/AuthProvider/AuthProvider';
+const Header = () => {
+
+/*   const {user,logOut} = useContext(AuthContext);
+  const navigate = useNavigate();
+  const handleSignOut = ()=>{
+    logOut()
+    .then(()=>     
+      navigate('/'))
+    .catch(err=>console.log(err))
+  } */
+
+    const menuItems = <>
+    <li>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+        <Link to='/services'>Services</Link>
+        <Link to='/blog'>Blog</Link>
+        <Link to='/contact'>Contact</Link>
+        {/* <Link to='/login'>Login</Link> */}
+    </li>
+    </>
+/*     const reviewItems = <>
+    <li>
+        <Link to='/addservice'>Add service</Link>
+        <Link to='/reviews'>My reviews</Link>
+    </li>
+    </> */
+    return (
+        <div className="navbar bg-base-100 pt-4">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            </label>
+            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 ">
+              {menuItems}
+            </ul>
+          </div>
+          <div className='flex'>
+          <Link to='/'>
+              <img className='h-10 mt-2 px-2' src={logo} alt="" />
+          </Link>
+          <Link to='/'>
+          <h1 className='text-2xl font-bold mt-3 pt-1'>iSells.com</h1>
+          </Link>
+          </div>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal p-0 font-semibold">
+          {menuItems}
+          </ul>
+        </div>
+        <div className="navbar-end">
+          
+        
+{/*           <div>
+            
+            {
+              user?.uid ?
+              <div className='flex justify-between'>
+            <div>
+          <ul className="menu menu-horizontal p-0 font-semibold">
+          {reviewItems}
+          </ul>
+        </div>
+        <div>
+        {
+            user?.photoURL && <img className='rounded-full w-8 h-8 flex mt-2' src={user.photoURL} alt="" /> 
+        }
+          </div>
+                <button onClick={handleSignOut} className="btn btn-outline text-cyan-50 bg-cyan-500 hover:bg-cyan-50 hover:text-cyan-500 hover:border-cyan-500">Logout</button>
+              </div>
+                      :
+                      <Link to='/login' className=""><button className="btn btn-outline mx-2 text-cyan-500 hover:bg-cyan-500 hover:text-cyan-50 hover:border-cyan-500">Login</button></Link>
+            }
+          </div> */}
+        </div>
+      </div>
+    );
+};
+
+export default Header;
