99d9ba821408392006bc8ab3a54017f29e50e18c
anhasamea <anhasamea707@gmail.com>
2022-11-26 21:12:23 +0600

    banner added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Banner = () => {
     return (
-        <div>
-            <h1>this is banner</h1>
-        </div>
+        <div className="p-6 py-12 mt-5 rounded-lg bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white">
+	<div className="container mx-auto">
+		<div className="flex flex-col lg:flex-row items-center justify-between">
+			<h2 className="text-center text-6xl tracking-tighter font-bold">Up to
+				<br className="sm:hidden"/>50% Off
+			</h2>
+			<div className="space-x-2 text-center py-2 lg:py-0">
+				<span>Plus free shipping! Use code:</span>
+				<span className="font-bold text-lg">iSELLS50</span>
+			</div>
+			<Link href="#" rel="noreferrer noopener" className="px-5 mt-4 lg:mt-0 py-3 rounded-md border block dark:bg-gray-50 dark:text-gray-900 dark:border-gray-400">Shop Now</Link>
+		</div>
+	</div>
+</div>
     );
 };
 
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -18,7 +18,7 @@ const Header = () => {
     <li>
         <Link to='/'>Home</Link>
         <Link to='/about'>About</Link>
-        <Link to='/services'>Services</Link>
+        {/* <Link to='/services'>Services</Link> */}
         <Link to='/blog'>Blog</Link>
         <Link to='/contact'>Contact</Link>
         
