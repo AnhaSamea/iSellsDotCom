fb2636f1dd81582aa9e1f94bfcb9b2a5fe7bfce3
anhasamea <anhasamea707@gmail.com>
2022-11-26 20:53:03 +0600

    private route created

--- src/Pages/Home/Category/PhoneModels/CategoryId.js
+++ src/Pages/Home/Category/PhoneModels/CategoryId.js
@@ -8,7 +8,7 @@ const CategoryId = () => {
 
     return (
         <div className='text-center '>
-            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4'>
                 {
                     models.models.map(model => <CategoryIdDetails key={model._id} model={model}></CategoryIdDetails>)
                 }
