0df2543dee7b3cb5c0b7d2bdf0ea650eb5d65b12
anhasamea <anhasamea707@gmail.com>
2022-11-28 09:26:13 +0600

    modal added

--- src/Pages/Home/Category/PhoneModels/CategoryIdDetails.js
+++ src/Pages/Home/Category/PhoneModels/CategoryIdDetails.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PrimaryButton from '../../../../Button/PrimaryButton';
 
 const CategoryIdDetails = ({ model }) => {
     const { picture, name, location, resale_price, } = model;
@@ -10,11 +11,38 @@ const CategoryIdDetails = ({ model }) => {
                 <p>Click the button to listen on Spotiwhy app.</p>
                 <p>Click the button to listen on Spotiwhy app.</p>
                 <p>Click the button to listen on Spotiwhy app.</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Purchase</button>
+                {/* modal */}
+
+                <PrimaryButton><label htmlFor="my-modal">Book Now</label></PrimaryButton>
+
+
+                {/* Put this part before </body> tag */}
+                <input type="checkbox" id="my-modal" className="modal-toggle" />
+                <div className="modal">
+                    <div className="modal-box">
+                        <form action="" className='bg-slate-100 my-5 py-8 px-5 rounded-lg'>
+                            <div>
+                                <label for="name" className="block mb-1 ml-1 text-start">Name</label>
+                                <input id="name" type="text" placeholder="Your name" required="" className="block w-full p-2 rounded focus:outline-none focus:ring focus:ring-opacity-25 focus:ring-violet-700 bg-slate-200" />
+                            </div>
+                            <div>
+                                <label for="email" className="block mb-1 ml-1 text-start">Email</label>
+                                <input id="email" type="email" placeholder="Your email" required="" className="block w-full p-2 rounded focus:outline-none focus:ring focus:ring-opacity-25 focus:ring-violet-700 bg-slate-200" />
+                            </div>
+                            <div>
+                                <label for="name" className="block mb-1 ml-1 text-start">Password</label>
+                                <input id="password" type="password" placeholder="password" required="" className="block w-full p-2 rounded focus:outline-none focus:ring focus:ring-opacity-25 focus:ring-violet-700 bg-slate-200" />
+                            </div>
+                            
+                        </form>
+                        <div className="modal-action">
+                            <PrimaryButton><label htmlFor="my-modal">Submit</label></PrimaryButton>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+
     );
 };
 
